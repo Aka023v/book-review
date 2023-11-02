9bbc69322a8326e053a1f8e61c4c6f6f46c23940
Aka023v <148590386+Aka023v@users.noreply.github.com>
2023-11-02 14:52:49 +0000

    修正

--- docker/mysql/docker-entrypoint-initdb.d/sample.sql
+++ docker/mysql/docker-entrypoint-initdb.d/sample.sql
@@ -1,17 +1,16 @@
 SET CHARSET UTF8;
-CREATE DATABASE IF NOT EXISTS twitter DEFAULT CHARACTER SET utf8;
-USE twitter;
+CREATE DATABASE IF NOT EXISTS book-review DEFAULT CHARACTER SET utf8;
+USE book-review;
 
-CREATE TABLE IF NOT EXISTS `tweet`
+CREATE TABLE IF NOT EXISTS `review`
 (
-  `id`       int(11) NOT NULL AUTO_INCREMENT,
-  `poster`   varchar(255) NOT NULL,
-  `tweet`    text,
+  `id`          int(11) NOT NULL AUTO_INCREMENT,
+  `poster`      varchar(255) NOT NULL,
+  `book_name`   varchar(255) NOT NULL,
+  `review`      text,
   PRIMARY KEY (`id`)
 );
 
-INSERT INTO `tweet`(`poster`, `tweet`) VALUES
-('さくら', '春が来た〜'),
-('もみじ', '秋が来た〜'),
-('あさがお', '夏が来た〜'),
-('つばき', '冬が来た〜');
+INSERT INTO `tweet`(`poster`, `book_name`, `review`) VALUES
+('さくら', 'ポケモン', 'キャラが可愛く、大人から子供まで楽しめると思います。'),
+('もみじ', '桃太郎', '幼稚園の子供に読み聞かせるのにいい本でした。');
